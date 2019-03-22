97959680b07a2fcf00925b2ee62966f597e1276e
Edson Silva-Filho <edsonsilva@Edsons-MacBook-Pro.local>
2019-03-22 14:49:00 -0500

    initial commitment

--- context.js
+++ context.js
@@ -20,6 +20,13 @@
   
 
 //Code Here
+let user = {
+  username: "Johnny",
+  email: "jonnybonny@wherever.go",
+  getUsername: function () {
+    return this.username
+  }
+}
 
 
 
@@ -43,7 +50,7 @@ class Animal {
 
 //Code Here
 
-
+let animal1 = new Animal;
 
 ////////// PROBLEM 3 //////////
 
@@ -62,6 +69,7 @@ let who = {
 }
 
 //Code Here
+let whoSaysHi = sayHi.bind(who);
 
 
 
@@ -78,7 +86,7 @@ function whatIsThis() {
 //Code Here
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 = "window"
 
 let product = {
   name: 'snake plant',
@@ -89,7 +97,7 @@ let product = {
 let func = whatIsThis.bind(product)
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+let context2 = product
 
 let vacation = {
   location: 'Hawaii',
@@ -100,7 +108,7 @@ let vacation = {
 }
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
+let context3 = vacation;
 
 
 class Family {
@@ -118,4 +126,4 @@ class Family {
 let family1 = new Family(2, 4, 1)
 
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-//let context4 = ???
+let context4 = family1
